bcbef772e5308bab667a14b115f4f7afd115882f
sebastian travez <sebastian.travez@megaprofer.com>
2022-04-24 09:13:36 -0500

    test person controller

--- src/test/java/com/pichincha/service/presentation/controller/PersonControllerTest.java
+++ src/test/java/com/pichincha/service/presentation/controller/PersonControllerTest.java
@@ -1,28 +1,56 @@
 package com.pichincha.service.presentation.controller;
 
+import com.pichincha.service.presentation.presenter.PersonPresenter;
+import com.pichincha.service.service.PersonService;
+import com.pichincha.service.util.TestData;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonControllerTest {
 
+    @InjectMocks
+    private PersonController personController;
+    @Mock
+    private PersonService personService;
+
+    private TestData testData;
+
     @BeforeEach
     public void setUp() {
+        testData = new TestData();
     }
 
     @Test
     public void savePerson() {
+        doNothing().when(personService).savePerson(any());
+        personController.savePerson(testData.personPresenter(testData.person()));
+        verify(personService).savePerson(any());
     }
 
     @Test
     public void getAllPerson() {
+        List<PersonPresenter> personPresenters = List.of(testData.personPresenter(testData.person()));
+        when(personService.getAllPersons()).thenReturn(personPresenters);
+        List<PersonPresenter> personPresenterList = personController.getAllPerson();
+        Assertions.assertThat(personPresenterList).isNotEmpty();
+        verify(personService).getAllPersons();
     }
 
     @Test
     public void deletePerson() {
+        doNothing().when(personService).deletePerson(any());
+        personController.deletePerson(UUID.randomUUID());
+        verify(personService).deletePerson(any());
     }
 }
---
+++ src/test/java/com/pichincha/service/util/TestData.java
@@ -0,0 +1,29 @@
+package com.pichincha.service.util;
+
+import com.pichincha.service.entity.Person;
+import com.pichincha.service.enums.GenderPerson;
+import com.pichincha.service.enums.IdentificationPattern;
+import com.pichincha.service.presentation.presenter.PersonPresenter;
+
+public class TestData {
+
+    public Person person() {
+        return Person.builder()
+                .fullName("TEST TEST")
+                .genderPerson(GenderPerson.MASCULINO)
+                .age(20)
+                .dni("1724022437")
+                .identificationPattern(IdentificationPattern.IDENTIFICATION_CARD)
+                .build();
+    }
+
+    public PersonPresenter personPresenter(Person person) {
+        return PersonPresenter.builder()
+                .fullName(person.getFullName())
+                .genderPerson(person.getGenderPerson())
+                .age(person.getAge())
+                .dni(person.getDni())
+                .identificationPattern(person.getIdentificationPattern())
+                .build();
+    }
+}
